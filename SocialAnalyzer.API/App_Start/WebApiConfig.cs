007fbf8ea1db31064756087d3334a7254329d974
Iuri Matos <iuriamatos@gmail.com>
2016-04-06 22:00:53 -0300

    add api project and make basic configurations. Also created the ping controller

---
+++ SocialAnalyzer.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SocialAnalyzer.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatters = formatters.JsonFormatter;
+            var settings = jsonFormatters.SerializerSettings;
+
+            settings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional});
+        }
+    }
+}
---
+++ SocialAnalyzer.API/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialAnalyzer.API.Controllers
+{
+    public class PingController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Working!");
+        }
+    }
+}
