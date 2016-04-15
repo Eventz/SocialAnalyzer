cc5582ad48758811196e591a337f1f2e07f70698
Núbio Knupp <nubioknupp@gmail.com>
2016-04-15 18:19:01 -0300

    Create method to find info of the user in GitHub.
    Create method to find info of the user in GitHub.

--- SocialAnalyzer.API/ServiceAgents/GitHubClient.cs
+++ SocialAnalyzer.API/ServiceAgents/GitHubClient.cs
@@ -1,26 +1,47 @@
-﻿using System.Net;
+﻿using SocialAnalyzer.API.ViewModels;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace SocialAnalyzer.API.ServiceAgents
 {
     public class GitHubClient
     {
-
-
         public IEnumerable<string> GetRepositories(string userLogin)
         {
             var url = $"users/{userLogin}/repos";
             IEnumerable<string> repositories;
+            var request = CreateRequest(url);
 
-            using (var response = CreateRequest(url))
+            using (var response = request.GetResponse() as HttpWebResponse)
             {
                 var content = GetResponseContent(response);
                 var json = JArray.Parse(content);
                 repositories = json.Select(item => item.Value<string>("full_name")).ToList();
             }
-
             return repositories;
         }
-                
+
+        public UserViewModel FindUserByEmail(string email)
+        {
+            var url = $"search/users?q={email}";
+            var user = new UserViewModel();
+            var request = CreateRequest(url);
+
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                var content = GetResponseContent(response);
+                var result = JsonConvert.DeserializeObject<dynamic>(content);
+                user.Email = email;
+                user.Login = result.login.ToString();
+            }
+            return user;
+        }
+
         private string GetResponseContent(HttpWebResponse response)
         {
             var reader = new StreamReader(response.GetResponseStream());
