9ea5e36702da2c3399016fab21efdaaf6050d051
Núbio Knupp <nubioknupp@gmail.com>
2016-04-15 17:58:09 -0300

    Create method to find repositories of the user in GitHub.
    Create method to find repositories of the user in GitHub.

--- SocialAnalyzer.API/ServiceAgents/GitHubClient.cs
+++ SocialAnalyzer.API/ServiceAgents/GitHubClient.cs
@@ -4,11 +4,34 @@ namespace SocialAnalyzer.API.ServiceAgents
 {
     public class GitHubClient
     {
-        public HttpWebRequest CreateRequest(string fill)
+
+
+        public IEnumerable<string> GetRepositories(string userLogin)
+        {
+            var url = $"users/{userLogin}/repos";
+            IEnumerable<string> repositories;
+
+            using (var response = CreateRequest(url))
+            {
+                var content = GetResponseContent(response);
+                var json = JArray.Parse(content);
+                repositories = json.Select(item => item.Value<string>("full_name")).ToList();
+            }
+
+            return repositories;
+        }
+                
+        private string GetResponseContent(HttpWebResponse response)
+        {
+            var reader = new StreamReader(response.GetResponseStream());
+            return reader.ReadToEnd();
+        }
+
+        private HttpWebRequest CreateRequest(string fill)
         {
             const string gitHubToken = "";
             const string urlApiGitHub = "https://api.github.com";
-            var url = $"{urlApiGitHub}/users/{fill}?access_token={gitHubToken}";
+            var url = $"{urlApiGitHub}/{fill}?access_token={gitHubToken}";
             var request = WebRequest.Create(url) as HttpWebRequest;
 
             if (request != null) request.UserAgent = "SocialAnalyzer";
